482fa391f30d990fb94c1ba3c0ba62054ab88231
Jeet singh <100671194+webdevjeet@users.noreply.github.com>
2024-10-16 15:51:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-let menuIcon = document.querySelector('#menu-icon'); 
-let navbar = document.querySelector('.navbar'); 
+document.addEventListener('DOMContentLoaded', () => {
+    const menuIcon = document.getElementById('menu-icon');
+    if (menuIcon) {
+        menuIcon.onclick = () => {
+            const navbar = document.querySelector('.navbar');
+            navbar.classList.toggle('active');
+        };
+    }
+});
 
-menuIcon.onclick = () => { 
-    menuIcon.classList.toggle('bx-x'); 
-    navbar.classList.toggle('active');
-}
+document.querySelector('.gradent-btn').addEventListener('click', function() {
+    window.open('https://wa.me/message/T35T5RQHZ5YJA1', '_blank');
+});
+
